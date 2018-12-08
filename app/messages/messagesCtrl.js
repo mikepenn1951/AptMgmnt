6ca4f81100a7cc51704e019bf9af686a0e798525
mikepenn1951 <mike.penn@gmail.com>
2018-12-08 22:15:15 +0200

    Fix Filters of text and Priority, fix sort by date, Add some messages to DB for testing of the filters and sortby

--- app/messages/messagesCtrl.js
+++ app/messages/messagesCtrl.js
@@ -1,4 +1,4 @@
-app.controller("messagesCtrl", function($scope,$http, messages, user, $location) {
+app.controller("messagesCtrl", function ($scope, $http, messages, user, $location) {
     // Checking if the user is currently logged in,
     // if not redirecting to the home page
     if (!user.isLoggedIn()) {
@@ -8,13 +8,43 @@ app.controller("messagesCtrl", function($scope,$http, messages, user, $location)
 
     messages.getMessages().then(function (messages) {
         $scope.messages = messages;
-    }, function(error) {
+    }, function (error) {
     })
 
-    $scope.isThisUserCM = function() {
+    $scope.isThisUserCM = function () {
         var utype = user.isUserCM();
         return utype;
     }
+
+    $scope.searchText = "";
+    $scope.filterMessage = function (message) {
+        // Check if the current name includes the value of searchText in the 
+        // properties fname or lname
+
+        if (message.title.toLowerCase().includes($scope.searchText.toLowerCase()) ||
+            message.description.toLowerCase().includes($scope.searchText.toLowerCase())) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    $scope.selP1 = "none";
+    $scope.filterPriority = function (message) {
+        // Check if the current name includes the value of searchText in the 
+        // properties fname or lname
+
+        if ( ($scope.selP1 == "none") ||
+             (message.priority == $scope.selP1) ) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    $scope.rdVar = "date";
 
 
 
