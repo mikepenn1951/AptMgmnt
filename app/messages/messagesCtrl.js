49306053ca9e5d8a648b622618b776fba87def0d
mikepenn1951 <mike.penn@gmail.com>
2018-12-09 14:43:53 +0200

    Add Update and Delete buttons to messages. Add logic to delete a msg. Start new message logic

--- app/messages/messagesCtrl.js
+++ app/messages/messagesCtrl.js
@@ -35,8 +35,8 @@ app.controller("messagesCtrl", function ($scope, $http, messages, user, $locatio
         // Check if the current name includes the value of searchText in the 
         // properties fname or lname
 
-        if ( ($scope.selP1 == "none") ||
-             (message.priority == $scope.selP1) ) {
+        if (($scope.selP1 == "none") ||
+            (message.priority == $scope.selP1)) {
             return true;
         } else {
             return false;
@@ -46,6 +46,31 @@ app.controller("messagesCtrl", function ($scope, $http, messages, user, $locatio
 
     $scope.rdVar = "date";
 
+    $scope.deleteMsg = function (message) {
+        // window.alert("over icon: " + ind);
+        // if ($scope.todoItems[ind].chbValue == false)
+        //   window.alert("Warning, Item is not completed: " + $scope.todoItems[ind].todo);
+        var ind = $scope.messages.indexOf(message);
+        $scope.messages.splice(ind, 1);
+    }
 
+    $scope.priority = "important";
+    $scope.saveNewMsg = function () {
+        var message = new $scope.Message();
+        message.id = 11;
+        message.date = 2009 / 03 / 23;
+        message.title = newmessage1t;
+        message.description = newmessage1d;
+        message.comments = newcomment1
+        message.imgUrl = "";
+        message.userId = 3;
+        messages.push(message);
+
+        window.alert("saveNewMsg " + messages );
+        // if ($scope.todoItems[ind].chbValue == false)
+        //   window.alert("Warning, Item is not completed: " + $scope.todoItems[ind].todo);
+        // var ind =  $scope.messages.indexOf(message);      
+        // $scope.messages.splice(ind, 1);
+    }
 
 });
