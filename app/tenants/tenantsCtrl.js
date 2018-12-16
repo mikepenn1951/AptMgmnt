f42d0768d11f64403e8507308eb6817bab2ca583
mikepenn1951 <mike.penn@gmail.com>
2018-12-16 12:26:09 +0200

    fix dynamic email service when creating a tenant, remove script definitions in index.htm (defined twice)

--- app/tenants/tenantsCtrl.js
+++ app/tenants/tenantsCtrl.js
@@ -15,10 +15,10 @@ app.controller("tenantsCtrl", function ($scope, $http, tenants, user, $location)
         // properties fname or lname
 
         if (tenant.fname.toLowerCase().includes($scope.searchTenText.toLowerCase()) ||
-        tenant.lname.toLowerCase().includes($scope.searchTenText.toLowerCase()) ||
-        tenant.email.toLowerCase().includes($scope.searchTenText.toLowerCase()) ||
-        tenant.addr.toLowerCase().includes($scope.searchTenText.toLowerCase()) ||
-        tenant.tel.toLowerCase().includes($scope.searchTenText.toLowerCase()) ) {
+            tenant.lname.toLowerCase().includes($scope.searchTenText.toLowerCase()) ||
+            tenant.email.toLowerCase().includes($scope.searchTenText.toLowerCase()) ||
+            tenant.addr.toLowerCase().includes($scope.searchTenText.toLowerCase()) ||
+            tenant.tel.toLowerCase().includes($scope.searchTenText.toLowerCase())) {
             return true;
         } else {
             return false;
@@ -29,15 +29,15 @@ app.controller("tenantsCtrl", function ($scope, $http, tenants, user, $location)
 
     // $scope.test = 55;
 
-    $scope.openDeleteTenModal = function(tenant){
+    $scope.openDeleteTenModal = function (tenant) {
         $("#delModal").modal();
         // $(".x").text( message.title );
         tenToDel = tenant;
     }
 
     $scope.deleteTen = function () {
-            var ind = $scope.tenants.indexOf(tenToDel);
-            $scope.tenants.splice(ind, 1);
+        var ind = $scope.tenants.indexOf(tenToDel);
+        $scope.tenants.splice(ind, 1);
     }
 
 
@@ -49,34 +49,40 @@ app.controller("tenantsCtrl", function ($scope, $http, tenants, user, $location)
     var updateOrSave = "save";
     var tenToUpdate;
     $scope.saveNewTen = function () {
-        if (updateOrSave == "save"){
+        if (updateOrSave == "save") {
+            var email = $scope.email;
+            var fname = $scope.fname;
+            var lname = $scope.lname;
             tenants.createTenant($scope.fname, $scope.lname, $scope.email,
                 $scope.addr, $scope.tel).then(function () {
 
-            // EmailJS
-            var template_params = {
-                to_email: user.getActiveUser().email,
-                from_name: "Apt Mngmnt",
-                to_name: user.getActiveUser().fname + " " + user.getActiveUser().lname,
-                message_html: "Welcome, you have been registered as a new tenant "
-            }
+                    // EmailJS
+                    var template_params = {
+                        to_email: email,
+                        from_name: "Apt Mngmnt",
+                        to_name: fname + " " + lname
+                    }
 
-            var service_id = "gmail";
-            var template_id = "template_Hsg1oCvb";
-            emailjs.send(service_id, template_id, template_params);
+                    var service_id = "default_service";
+                    var template_id = "tn";
+                    emailjs.send(service_id, template_id, template_params).then(function(response) {
+                        console.log(response);
+                    }, function(error) {
+                        console.error(error);
+                    });
 
-                $location.path("/tenants")
-            }, function (err) {
-                console.log(err);
-            })
-            
-        }else{
+                    $location.path("/tenants")
+                }, function (err) {
+                    console.log(err);
+                })
+
+        } else {
             tenants.updateTenant($scope.fname, $scope.lname, $scope.email,
                 $scope.addr, $scope.tel, tenToUpdate).then(function () {
-                $location.path("/tenants")
-            }, function (err) {
-                console.log(err);
-            })
+                    $location.path("/tenants")
+                }, function (err) {
+                    console.log(err);
+                })
         }
         updateOrSave = "save";
         $scope.fname = "";
@@ -84,7 +90,7 @@ app.controller("tenantsCtrl", function ($scope, $http, tenants, user, $location)
         $scope.email = "";
         $scope.addr = "";
         $scope.tel = "";
-        
+
     }
 
     $scope.updateTen = function (tenant) {
