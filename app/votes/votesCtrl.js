79661d287b1ccf116c2a4bc281197b695ec76579
mikepenn1951 <mike.penn@gmail.com>
2019-03-04 22:02:34 +0200

    radio button, add $parent to name in ng-model

--- app/votes/votesCtrl.js
+++ app/votes/votesCtrl.js
@@ -1,5 +1,5 @@
 app.controller("votesCtrl", function ($scope, $http, votes, user, $location) {
-    
+    $scope.voteR = 0;
     if (!user.isLoggedIn()) {
         $location.path("/");
         return;
