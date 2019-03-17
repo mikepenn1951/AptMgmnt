496e1a2cc88fbd5f839f0f73b135473779580fdf
mikepenn1951 <mike.penn@gmail.com>
2019-03-17 18:24:25 +0200

    allow checkbox array

--- app/votes/votesCtrl.js
+++ app/votes/votesCtrl.js
@@ -1,5 +1,6 @@
 app.controller("votesCtrl", function ($scope, $http, votes, user, $location) {
     $scope.voteR = 0;
+    $scope.voteC = 0;
     if (!user.isLoggedIn()) {
         $location.path("/");
         return;
